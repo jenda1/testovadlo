cf051b2c6c911bf9fccc56a2f6ee77efd9501f6e
Jan Lana <lana.jan@gmail.com>
2017-11-30 00:09:49 +0100

    fix checkstyle

--- tasks/checkstyle.sh
+++ tasks/checkstyle.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-cd /data/arg0
+java_files=( $(find /data/arg0/ -type f -name '*.java' -print) )
 
-java_files="$(find . -type f -name '*.java' -print)"
-
-[[ -z "$java_files" ]] || { \
-	echo "$ checkstyle $java_files"
-	out=$(java -jar /lib/checkstyle-8.5-all.jar -c /lib/style.xml $java_files) || {
+[[ -z "${java_files[@]}" ]] || { \
+	echo "$ checkstyle ${java_files[@]#/data/arg0/}"
+	out=$(java -jar /lib/checkstyle-8.5-all.jar -c /lib/style.xml "${java_files[@]}") || {
 		grep -v '^\(Starting\|Audit done\|Checkstyle ends\)' <<<"$out"
 		exit 1
 	}
