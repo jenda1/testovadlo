f9fa9f591e6942b485ee185f8d7cd376ca3a6296
Jan Lana <lana.jan@gmail.com>
2017-11-30 08:53:07 +0100

    do not fail if a test fails if exit code is less that 100

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,10 @@ set -e
 
 for t in $(find /test.d/ -executable ! -type d | sort -n)
 do
-	$t
+	$t || echo -e "\n** chyba: $?"
 done
 
 if [[ -f /test && -x /test ]]
 then
-	/test
+	/test || echo -e "\nchyba: $?"
 fi
