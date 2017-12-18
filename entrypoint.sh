44ae94da59042169c2652201d0de257e906c27d9
jenda1 <lana.jan@gmail.com>
2017-12-18 16:56:37 +0100

    Update entrypoint.sh
    exit if return code is 10 or higher

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,9 @@ set -e
 
 for t in $(find /test.d/ -executable ! -type d | sort -n)
 do
-	$t || { ret=$?; echo -e "\n** chyba: $ret"; [[ $ret -lt 100 ]] || exit 1; }
+	$t
+        ret=$?
+        
+        echo $ret > $t.exitcode
+        [[ $ret -le 10 ]] || exit $((ret-10))
 done
-
-if [[ -f /test && -x /test ]]
-then
-	/test || { ret=$?; echo -e "\n** chyba: $ret"; [[ $ret -lt 100 ]] || exit 1; }
-fi
