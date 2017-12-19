fcf04fc254e4218065fdf55ad175f797308bfad1
Jan Lana <lana.jan@gmail.com>
2017-12-19 08:23:37 +0100

    fix entrypoint.sh to fail for exitcode >= 10 only

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,8 @@ set -e
 
 for t in $(find /test.d/ -executable ! -type d | sort -n)
 do
-	$t
-        ret=$?
-        
+        $t && ret=0 || ret=$?
+
         echo $ret > $t.exitcode
         [[ $ret -le 10 ]] || exit $((ret-10))
 done
