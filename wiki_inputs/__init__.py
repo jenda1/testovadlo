f6a1f53e02c0ad30b2039cbebbdc8afafcfd385c
Jan Lana <lana.jan@gmail.com>
2018-12-03 10:34:09 +0100

    add support for files

--- wiki_inputs/__init__.py
+++ wiki_inputs/__init__.py
@@ -4,10 +4,13 @@ import sys
 import pathlib
 import re
 import logging
+import base64
+import os
 
 logger = logging.getLogger(__package__)
 
 path_arg = pathlib.Path("/arg")
+
 
 def main():
     parser = argparse.ArgumentParser(prog="wiki_inputs", description="wiki_inputs helper")
@@ -17,41 +20,81 @@ def main():
                         action="count", default=0,
                         help="increases log verbosity for each occurence.")
 
-    get_parser = subparsers.add_parser('get')
+    get_parser = subparsers.add_parser('get', description="get argument or input value")
+    get_parser.add_argument("--owner", required=False, help="owner name or _group name_ or _all_")
     get_parser.add_argument("arg", help="argument")
-    arguments = parser.parse_args()
 
+    get_parser = subparsers.add_parser('h2p', description="get argument or input value and transform hodnoceni to percents")
+    get_parser.add_argument("--owner", required=False, help="owner name or _group name_ or _all_")
+    get_parser.add_argument("arg", help="argument")
+
+    arguments = parser.parse_args()
     logger.setLevel(max(3 - arguments.verbose_count, 0) * 10)
 
+    out = None
     if arguments.command == 'get':
-        try:
-            print(get(arguments.arg))
-        except:
-            sys.exit(1)
+        out = get(arguments.arg, arguments.owner)
+    elif arguments.command == 'h2p':
+        out = h2p(arguments.arg, arguments.owner)
+
+    if type(out) == list:
+        print("\n".join(out))
+    elif out is not None:
+        print(str(out))
 
 
-def get(name, user=None):
+def get(name, owner=None):
     fn = str(name).replace("/", "_")
-    if user:
-        fn += "@" + str(user)
+    if owner:
+        try:
+            owner = get(str(int(owner)))
+        except ValueError:
+            pass
+
+        fn += "@" + str(owner)
 
     try:
         with open(path_arg/f"{fn}.json", "r") as fh:
             val = json.load(fh)
-
-        return val['val']
-
     except FileNotFoundError:
         if type(name) != int:
-            print("#WI_NATIVE " + json.dumps({'type':'getval', 'val':str(name), 'user':user, 'id':fn}))
+            print("#WI_NATIVE " + json.dumps({'type': 'getval',
+                                              'val': str(name),
+                                              'user': owner,
+                                              'id': fn}),
+                  file=sys.stderr)
+        return
 
-    return None
+    if val['type'] is None:
+        return None
+
+    elif val['type'] in ["str", "int", "float"]:
+        return val['val']
+
+    elif val['type'] in ["file", "files"]:
+        out = list()
+        curr = os.path.realpath(os.getcwd())
+
+        if val['val']:
+            for fl in val['val']:
+                d = os.path.realpath(os.path.join(curr, fl['name']))
+                assert os.path.commonprefix([curr, d]) == curr
+
+                with open(d, "wb") as fd:
+                    out.append(d)
+                    fd.write(base64.b64decode(fl['content']))
+
+        return out
+
+    else:
+        return val['val']
 
 
-hodnoceni_re = re.compile("\((?P<op>[+=-]?)(?P<num>\d+)\)")
+hodnoceni_re = re.compile(r"\((?P<op>[+=-]?)(?P<num>\d+)\)")
 
-def hodnoceni2procenta(name, user=None):
-    val = get(name, user)
+
+def hodnoceni2procenta(name, owner=None):
+    val = get(name, owner)
 
     if val is None:
         return None
@@ -73,5 +116,6 @@ def hodnoceni2procenta(name, user=None):
 
     return int(out)
 
-def h2p(name, user=None):
-    return hodnoceni2procenta(name, user)
+
+def h2p(name, owner=None):
+    return hodnoceni2procenta(name, owner)
